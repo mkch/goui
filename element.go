c0bba182f20c79c4c1589281008ee2f739b5afd0
Kevin Yuan <farproc@gmail.com>
2025-11-20 10:02:31 +0800

    Refactor: Move layouter related code to layouter.go

--- element.go
+++ element.go
@@ -1,10 +1,8 @@
 package goui
 
 import (
-	"iter"
 	"reflect"
 	"slices"
-	"time"
 
 	"github.com/mkch/goui/native"
 )
@@ -164,111 +162,6 @@ func (e *NativeElement) NativeHandle(*Context) native.Handle {
 func (e *NativeElement) destroy() {
 	if e.DestroyFunc != nil {
 		e.DestroyFunc(e.Handle)
-	}
-}
-
-// debugLayouterVer records a debug layouter and its highlight version.
-type debugLayouterVer struct {
-	Layouter *debugLayouter
-	Version  uintptr
-}
-
-// debugLayouter is a [Layouter] wrapper that records debugging information.
-type debugLayouter struct {
-	Layouter
-	Ctx                  *Context
-	Size                 Size                // Last computed size
-	Pos                  Point               // Last computed position
-	Highlight            bool                // Whether to highlight the outline of this layouter
-	HighlightVer         uintptr             // Version of the highlight, used to avoid redundant redraws
-	CancelHighlightBatch *[]debugLayouterVer // Batch of layouters to cancel highlight together
-}
-
-func (l *debugLayouter) Layout(ctx *Context, constraints Constraints) (size Size, err error) {
-	l.Highlight = true // Mark to highlight
-	l.HighlightVer++
-
-	if debugParent, ok := l.parent().(*debugLayouter); ok && // parent is debug layouter but can be nil
-		debugParent.CancelHighlightBatch != nil && *debugParent.CancelHighlightBatch != nil {
-		// Inherit and join the cancel highlight batch from parent
-		l.CancelHighlightBatch = debugParent.CancelHighlightBatch
-		*l.CancelHighlightBatch = append(*l.CancelHighlightBatch, debugLayouterVer{Layouter: l, Version: l.HighlightVer})
-	} else {
-		// This is the root of laying out
-		l.CancelHighlightBatch = &[]debugLayouterVer{{Layouter: l, Version: l.HighlightVer}}
-		defer func() {
-			if err != nil {
-				return // do not show highlight if layout fails
-			}
-			// Show highlight after laying out(include children) is done
-			native.InvalidWindow(l.Ctx.window.Handle)
-			// Schedule canceling all highlights in the batch after a delay
-			const delay = 100 * time.Millisecond
-			batch := *l.CancelHighlightBatch
-			*l.CancelHighlightBatch = nil
-			time.AfterFunc(delay, func() {
-				l.Ctx.app.Post(func() {
-					// Cancel all highlights in a batch
-					var cancelled bool
-					for _, record := range batch {
-						if record.Version < record.Layouter.HighlightVer {
-							continue // too late, already updated
-						}
-						record.Layouter.Highlight = false
-						record.Layouter.HighlightVer = 0
-						cancelled = true
-					}
-					// Request a redraw to remove the highlights
-					if cancelled {
-						native.InvalidWindow(l.Ctx.window.Handle)
-					}
-				})
-			})
-		}()
-	}
-
-	size, err = l.Layouter.Layout(ctx, constraints)
-	if err != nil {
-		return
-	}
-	l.Size = size // Record size
-	return
-}
-
-func (l *debugLayouter) PositionAt(x, y int) (err error) {
-	err = l.Layouter.PositionAt(x, y)
-	if err != nil {
-		return
-	}
-	l.Pos = Point{X: x, Y: y} // Record position
-	return
-}
-
-// allLayouterDebugOutlines returns an iterator of debug rectangles for the given layouter tree.
-// The tree must be built with debugging([Window.DebugLayout]) on.
-func allLayouterDebugOutlines(root Layouter) iter.Seq[native.DebugRect] {
-	return func(yield func(native.DebugRect) bool) {
-		// Use a stack to avoid recursive iterator calls
-		stack := []Layouter{root}
-		for len(stack) > 0 {
-			current := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-
-			if debugLayouter, ok := current.(*debugLayouter); ok {
-				if !yield(native.DebugRect{
-					Left:      debugLayouter.Pos.X,
-					Top:       debugLayouter.Pos.Y,
-					Right:     debugLayouter.Pos.X + debugLayouter.Size.Width,
-					Bottom:    debugLayouter.Pos.Y + debugLayouter.Size.Height,
-					Highlight: debugLayouter.Highlight}) {
-					return
-				}
-				// Add children to stack in reverse order to maintain left-to-right traversal
-				for i := debugLayouter.NumChildren() - 1; i >= 0; i-- {
-					stack = append(stack, debugLayouter.Child(i))
-				}
-			}
-		}
 	}
 }
 
